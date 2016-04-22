c9b4f3c6bc960f05f75dbcf24721ade49252ad57
Noah Maxey <nmaxey@purdue.edu>
2016-04-22 18:23:16 +0000

    made ingame.js

---
+++ public/ingame.js
@@ -0,0 +1,20 @@
+/*eslint-env browser */
+function resizeCanvas(){
+
+  var canvas = document.getElementById('playerCanvas');
+
+  var width  = window.innerWidth;
+  var height = window.innerHeight;
+
+  width = width - 200;
+  var temp = width % 10;
+  width = width - temp;
+
+  height = height - 200;
+  temp = height % 10;
+  height = height - temp;
+
+  canvas.width = width;
+  canvas.height = height;
+
+}
