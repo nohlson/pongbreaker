7732d25a025543a7fcc10289c4d1c15a2c5d1af8
Garrett Davidson <gdavids@purdue.edu>
2016-04-22 14:50:34 -0400

    Added renamed file

---
+++ public/.#lobby.html
@@ -0,0 +1,1 @@
+garrett@pal-nat186-76-205.itap.purdue.edu.8727
---
+++ public/lobby.js
@@ -0,0 +1,36 @@
+function connect(username) {
+    console.log("Beginning connect");
+    var server = require("net").createServer();  
+    var io = require("socket.io")(server);
+
+    var handleClient = function (socket) {  
+	// we've got a client connection
+	socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
+    };
+
+    io.on("connection", handleClient);
+
+    server.listen(80);
+    console.log("Finished connect");
+}
+
+function addRow(userName)
+{
+    //If getElementsByTagName isn't defined then return
+    if (!document.getElementsByTagName) return;
+
+    newRow = document.createElement("tr");
+    newUserName = document.createElement("td");
+    newPlayButtonColumn = document.createElement("td");
+    newPlayButton = document.createElement("button");
+    newPlayButtonColumn.appendChild(newPlayButton);
+    newPlayButton.innerText = "Play";
+    newUserName.innerText = userName;
+
+    newRow.appendChild(newUserName);
+    newRow.appendChild(newPlayButtonColumn);
+
+    tableBody = document.getElementsByTagName("tbody")[0];
+    tableBody.appendChild(newUserName);
+    tableBody.appendChild(newPlayButtonColumn);
+}
