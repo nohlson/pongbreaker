9ae46be51d1d55dae686b4c0cda84c5a12afaa25
Noah Maxey <nmaxey@purdue.edu>
2016-04-22 17:14:24 +0000

    Made ingame

--- public/index.js
+++ public/index.js
@@ -2,7 +2,9 @@
 
 
 
+/*eslint-env node, browser*/
 function play_button_click() {
-	console.log("Button click")
+	console.log("Button click");
 	window.open("lobby.html", '_blank');
+	window.open("ingame.html", '_blank');
 }
