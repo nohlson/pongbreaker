918a03fa744d1e179713664f675bb9c148b0e31e
Garrett Davidson <gdavids@purdue.edu>
2016-04-22 15:29:26 -0400

    Migrated to app.js

--- app.js
+++ app.js
@@ -27,3 +27,41 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+function connect(username) {
+    console.log("Beginning connect");
+    var server = require("net").createServer();  
+    var io = require("socket.io")(server);
+
+    var handleClient = function (socket) {  
+	// we've got a client connection
+	socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
+    };
+
+    io.on("connection", handleClient);
+
+    server.listen(80);
+    console.log("Finished connect");
+}
+
+function addRow(userName)
+{
+    //If getElementsByTagName isn't defined then return
+    if (!document.getElementsByTagName) return;
+
+    newRow = document.createElement("tr");
+    newUserName = document.createElement("td");
+    newPlayButtonColumn = document.createElement("td");
+    newPlayButton = document.createElement("button");
+    newPlayButtonColumn.appendChild(newPlayButton);
+    newPlayButton.innerText = "Play";
+    newUserName.innerText = userName;
+
+    newRow.appendChild(newUserName);
+    newRow.appendChild(newPlayButtonColumn);
+
+    tableBody = document.getElementsByTagName("tbody")[0];
+    tableBody.appendChild(newUserName);
+    tableBody.appendChild(newPlayButtonColumn);
+}
+
