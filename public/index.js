e10cf084e3b2e72487942f0ab16be8cc6824eb52
nohlson <nateohlson@gmail.com>
2016-04-24 18:55:31 -0400

    index now main onboarding page

--- public/index.js
+++ public/index.js
@@ -1,20 +1,20 @@
 //index.js
+
 var lobbyjs = require('lobby');
 
 
 /*eslint-env node, browser*/
 function play_button_click() {
 	console.log("Button click");
-	window.open("lobby.html", '_blank');
-	window.open("ingame.html", '_blank');
+	
 }
 
-function connect(username) {
+function connect() {
     console.log("Beginning connect");
 
     var socket = io();
     var username = document.getElementById("username").value
     socket.emit("newuserconnect", {user: username});
-   
+    window.open("ingame.html", '_blank');  
     console.log("Finished connect");
 }
