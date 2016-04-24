dc2a7192382c5af22d49a2acd0081793995a4f7e
nohlson <nateohlson@gmail.com>
2016-04-24 19:07:05 -0400

    removed dependency for lobby

--- public/index.js
+++ public/index.js
@@ -1,14 +1,5 @@
 //index.js
-
-var lobbyjs = require('lobby');
-
-
 /*eslint-env node, browser*/
-function play_button_click() {
-	console.log("Button click");
-	
-}
-
 function connect() {
     console.log("Beginning connect");
 
