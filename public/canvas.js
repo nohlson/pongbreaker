a50905bbd9de9a8023865a2d2dd9530c80efdd64
Noah Maxey <nmaxey@purdue.edu>
2016-04-26 04:11:00 +0000

    goes to scoreboard after 3 secs

--- public/canvas.js
+++ public/canvas.js
@@ -328,11 +328,17 @@ function connect() {
     
     socket.on('scoresave', function(data) {
 		$.ajax( { url: "/save_score?name=" + username + "&score=" + data.p1score + "&name1=" + opusername + "&score1=" + data.p2score , cache : false }).done(function() {    
-    		window.location.replace("/index.html"); // Go to hiscore page
+    	setTimeout(function(){ 
+    		window.location.replace("/scoreboard.html"); // Go to hiscore page
+        
+    	}, 3000); 
   		});
     });
-    socket.on('gameover', function(data) {   
-    	window.location.replace("/index.html"); // Go to hiscore page
+    socket.on('gameover', function(data) {
+    	setTimeout(function(){ 
+    		window.location.replace("/scoreboard.html"); // Go to hiscore page
+        
+    	}, 3000); 
 
     });
 
